6a77463490d17a541d17a71ab7385d5e579f73fb
eakpo <97016093+erhimeakpo@users.noreply.github.com>
2022-04-04 18:59:24 -0400

    Added while loop to play again

--- main.py
+++ main.py
@@ -2,35 +2,43 @@
 
 import random as rand
 
-choices = ['rock', 'paper', 'scissors']
-player = None
-computer = rand.choice(choices)
+while True:
+    choices = ['rock', 'paper', 'scissors']
+    player = None
+    computer = rand.choice(choices)
 
-while player not in choices:
-    player = input('Rock, Paper, Scissors?').lower().strip()
+    while player not in choices:
+        player = input('Rock, Paper, Scissors?').lower().strip()
 
-if player == computer:
-    print("It's a tie!")
+    if player == computer:
+        print("It's a tie!")
 
-elif player == 'Rock'.lower():
-    if computer == 'Paper'.lower():
-        print('You lose!')
-    elif computer == 'Scissors'.lower():
-        print('You win!')
+    elif player == 'rock':
+        if computer == 'paper':
+            print('You lose!')
+        elif computer == 'scissors':
+            print('You win!')
 
-elif player == 'Paper'.lower():
-    if computer == 'Scissors'.lower():
-        print('You lose!')
-    elif computer == 'Rock'.lower():
-        print('You win!')
+    elif player == 'paper':
+        if computer == 'scissors':
+            print('You lose!')
+        elif computer == 'rock':
+            print('You win!')
 
-elif player == 'Scissors'.lower():
-    if computer == 'Paper'.lower():
-        print('You win!')
-    elif computer == 'Rock'.lower():
-        print('You lose!')
+    elif player == 'scissors':
+        if computer == 'paper':
+            print('You win!')
+        elif computer == 'rock':
+            print('You lose!')
 
-print('Computer: ' + computer.capitalize() + ' \nPlayer: ' + player.capitalize())
+    print('Computer: ' + computer.capitalize() + ' \nPlayer: ' + player.capitalize())
+
+    play_again = input("Play again? (Yes/No): ").lower()
+
+    if play_again != 'yes':
+        break
+
+print('Bye!')
 
 
 
